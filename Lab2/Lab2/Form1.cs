88c6bf3bc9d4143c1c2fc8a8cc2563e5aa5f72f7
Lancelei <Lancelei@hotmail.ca>
2017-01-22 22:33:42 -0800

    Added keyboard funcitonality.

--- Lab2/Lab2/Form1.cs
+++ Lab2/Lab2/Form1.cs
@@ -13,8 +13,7 @@ namespace Lab2
 {
     public partial class Form1 : Form
     {
-        int result = 0;
-        int oldResult = 0;
+        float result = 0;
         String operation = "";
         bool calculated = false;
         bool awake = false;
@@ -24,24 +23,8 @@ namespace Lab2
             InitializeComponent();
 
             // Disable buttons before turning calculator on
-            button0.Enabled = false;
-            button1.Enabled = false;
-            button2.Enabled = false;
-            button3.Enabled = false;
-            button4.Enabled = false;
-            buttonAdd.Enabled = false;
-            button13.Enabled = false;
-            button12.Enabled = false;
-            button11.Enabled = false;
-            ButtonC.Enabled = false;
-            ButtonOff.Enabled = false;
-            buttonEquals.Enabled = false;
-            button8.Enabled = false;
-            button9.Enabled = false;
-            ButtonCE.Enabled = false;
-            button7.Enabled = false;
-            button6.Enabled = false;
-            button5.Enabled = false;
+            turn_OffCalculator();
+
 
         }
 
@@ -72,7 +55,7 @@ namespace Lab2
         {
             Button button = (Button)sender;
             operation = button.Text;
-            result = Int32.Parse(DisplayBox.Text);
+            result = float.Parse(DisplayBox.Text);
             DisplayBox.Clear();
             if(calculated == true)
             {
@@ -81,7 +64,7 @@ namespace Lab2
             
         }
 
-        public void calculate(int val)
+        public void calculate(float val)
         {
             // Determine result with the chosen operation
             switch (operation)
@@ -105,7 +88,7 @@ namespace Lab2
         private void buttonEquals_Click(object sender, EventArgs e)
         {
             // Calculate the value based on the operation and operand
-            calculate(Int32.Parse(DisplayBox.Text));
+            calculate(float.Parse(DisplayBox.Text));
             DisplayBox.Clear();
             DisplayBox.Text += result.ToString();
             calculated = true;
@@ -139,30 +122,7 @@ namespace Lab2
                         // Set the flag to true and evaluate in KeyPress event.
                         nonNumberEntered = true;
                     }
-                    switch(e.KeyCode)
-                    {
-                        case Keys.Add:
-                            operation = "+";
-                            DisplayBox.Clear();
-                            break;
-                        case Keys.Subtract:
-                            operation = "-";
-                            DisplayBox.Clear();
-                            break;
-                        case Keys.Multiply:
-                            operation = "*";
-                            DisplayBox.Clear();
-                            break;
-                        case Keys.Divide:
-                            operation = "/";
-                            DisplayBox.Clear();
-                            break;
-                        case Keys.Enter:
-                            calculate(Int32.Parse(DisplayBox.Text));
-                            DisplayBox.Clear();
-                            DisplayBox.Text = result.ToString();
-                            break;
-                    }
+
                 }
             }
             //If shift key was pressed, it's not a number.
@@ -176,15 +136,83 @@ namespace Lab2
         // characters from entering the control.
         private void DisplayBox_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
         {
+            if(awake == false)
+            {
+                DisplayBox.Clear();
+                result = 0;
+                calculated = false;
+                awake = true;
+            }
             // Check for the flag being set in the KeyDown event.
             if (nonNumberEntered == true)
             {
+                // Determine what calculator function will be executed
+                // Implement and execute the equals button
+                if (e.KeyChar == Convert.ToChar(Keys.Enter))
+                {
+                    calculate(float.Parse(DisplayBox.Text));
+                    DisplayBox.Clear();
+                    DisplayBox.Text += result.ToString();
+
+                }
+                // Implement and execute technical functions of calculator
+                else if(e.KeyChar == 'h' || e.KeyChar == 'j' || e.KeyChar == 'k' || e.KeyChar == 'l')
+                {
+                    switch (e.KeyChar)
+                    {
+                        case 'h': // on button
+                            turn_OnCalculator();
+                            awake = false;
+                            break;
+                        case 'j': // off button
+                            turn_OffCalculator();
+                            break;
+                        case 'k': // C Function
+                            DisplayBox.Clear();
+                            break;
+                        case 'l': // CE Function
+                            turn_OnCalculator();
+                            break;
+                    }
+                }
+                // Implement and execute operator and calculate the result
+                else
+                {
+                    switch (e.KeyChar)
+                    {
+                        case '+':
+                            operation = "+";
+                            break;
+                        case '-':
+                            operation = "-";
+                            break;
+                        case '*':
+                            operation = "*";
+                            break;
+                        case '/':
+                            operation = "/";
+                            break;
+                        
+                    }
+                    result = float.Parse(DisplayBox.Text);
+                    DisplayBox.Clear();
+                }
                 // Stop the character from being entered into the control since it is non-numerical.
                 e.Handled = true;
             }
         }
 
         private void ButtonOn_Click(object sender, EventArgs e)
+        {
+            turn_OnCalculator();
+        }
+
+        private void ButtonOff_Click(object sender, EventArgs e)
+        {
+            turn_OffCalculator();
+        }
+
+        private void turn_OnCalculator()
         {
             DisplayBox.Text = "0";
             button0.Enabled = true;
@@ -206,9 +234,9 @@ namespace Lab2
             button6.Enabled = true;
             button5.Enabled = true;
             DisplayBox.Focus();
-        }
 
-        private void ButtonOff_Click(object sender, EventArgs e)
+        }
+        private void turn_OffCalculator()
         {
             DisplayBox.Clear();
             button0.Enabled = false;
@@ -229,6 +257,8 @@ namespace Lab2
             button7.Enabled = false;
             button6.Enabled = false;
             button5.Enabled = false;
+            awake = false;
+
         }
         private void buttonAdd_Click(object sender, EventArgs e)
         {
